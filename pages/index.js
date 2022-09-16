3829bfdbaf2a4dfe6e89d8bfa03661a263278760
muratdem1r <59487469+muratdem1r@users.noreply.github.com>
2022-09-16 23:49:16 +0300

    tailwindcss install

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to my portfolio Murat Demirr</h1>
+        <h1 className="text-red-900 text-3xl">
+          Welcome to my portfolio Murat Demirr
+        </h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
